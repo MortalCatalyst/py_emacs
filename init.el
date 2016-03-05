9ddc76c42729bf3deeb89bf33676389d6bef6323
mortalcatalyst <flebber.crue@gmail.com>
2016-03-05 23:18:39 +1100

    added switch panes option

--- init.el
+++ init.el
@@ -41,6 +41,12 @@
 (load-theme 'leuven t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
 
+;;switch panes
+;; use Shift+arrow_keys to move cursor around split panes
+(windmove-default-keybindings)
+;; when cursor is on edge, move to the other side, as in a torus space
+(setq windmove-wrap-around t )
+
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
