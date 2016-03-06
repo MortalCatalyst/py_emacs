2222caab5114a3d4b83bedd559d9899c17830e28
mortalcatalyst <flebber.crue@gmail.com>
2016-03-07 10:46:42 +1100

    pre-merge

--- init.el
+++ init.el
@@ -117,6 +117,52 @@
 (setq web-mode-enable-current-element-highlight t)
 (setq web-mode-enable-current-column-highlight t)
 
+(defun xah-cut-line-or-region ()
+  "Cut current line, or text selection.
+When `universal-argument' is called first, cut whole buffer (respects `narrow-to-region').
+
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-06-10"
+  (interactive)
+  (if current-prefix-arg
+      (progn ; not using kill-region because we don't want to include previous kill
+        (kill-new (buffer-string))
+        (delete-region (point-min) (point-max)))
+    (progn (if (use-region-p)
+               (kill-region (region-beginning) (region-end) t)
+             (kill-region (line-beginning-position) (line-beginning-position 2))))))
+
+(defun xah-copy-line-or-region ()
+  "Copy current line, or text selection.
+When called repeatedly, append copy subsequent lines.
+When `universal-argument' is called first, copy whole buffer (respects `narrow-to-region').
+
+URL `http://ergoemacs.org/emacs/emacs_copy_cut_current_line.html'
+Version 2015-12-30"
+  (interactive)
+  (let (ξp1 ξp2)
+    (if current-prefix-arg
+        (setq ξp1 (point-min) ξp2 (point-max))
+      (if (use-region-p)
+          (setq ξp1 (region-beginning) ξp2 (region-end))
+        (setq ξp1 (line-beginning-position) ξp2 (line-end-position))))
+    (if (eq last-command this-command)
+        (progn
+          ;; (push-mark (point) "NOMSG" "ACTIVATE")
+          (kill-append "\n" nil)
+          (forward-line 1)
+          (end-of-line)
+          (kill-append (buffer-substring-no-properties (line-beginning-position) (line-end-position)) nil)
+          (message "Line copy appended"))
+      (progn
+        (kill-ring-save ξp1 ξp2)
+        (if current-prefix-arg
+            (message "Buffer text copied")
+          (message "Text copied"))))))
+
+(global-set-key (kbd "<f2>") 'xah-cut-line-or-region) ; cut
+(global-set-key (kbd "<f3>") 'xah-copy-line-or-region) ; copy
+(global-set-key (kbd "<f4>") 'yank) ; paste
 
 ;; init.el ends here
 (custom-set-variables
